6800705899637d5da6d4fe33fab5a04e91aa66aa
Regi25 <reginaiuldasheva@gmail.com>
2022-02-24 17:37:28 +0100

    1 file modified

--- soubor/ukol1.js
+++ soubor/ukol1.js
@@ -3,4 +3,4 @@ let vek = 34;
 let vaha = 65;
 let vyska = 1.67;
 let bmi = vaha / vyska;
-console.log("Jmenuji se" + jmeno + "je mi" + vek + "vazim" + vaha + "kg"+ "a moje BMI je" + bmi");*/
+console.log("Jmenuji se" + jmeno + "je mi" + vek + "vazim" + vaha + "kg"+ "a moje BMI je" + bmi");
