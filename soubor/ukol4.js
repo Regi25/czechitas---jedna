68cfb5f34cbe2640d72d1e3678f05a3386568752
Regi25 <reginaiuldasheva@gmail.com>
2022-02-24 18:05:26 +0100

    1 file modified

--- soubor/ukol4.js
+++ soubor/ukol4.js
@@ -3,7 +3,7 @@ na číselné ose celých čísel – včetně.
 Pokud tedy zadá 3 a 7, uvažujme čísla 3, 4, 5, 6, 7.
 Vypište pomocí FOR*/
 for (let a = 1; a <=10; a++);
- for (let b = 10; b = b + 10) {
+ for (let b = 10; b = b + 10); {
    console.log("A:" + a);
    console.log("B:" + b);
    }  
