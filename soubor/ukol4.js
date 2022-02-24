d5c743595154042298cc886997b3edab5cda03ee
Regi25 <reginaiuldasheva@gmail.com>
2022-02-24 18:18:38 +0100

    1 file modified

--- soubor/ukol4.js
+++ soubor/ukol4.js
@@ -1,10 +1,15 @@
 /*Nechte uživatele zadat 2 čísla, které značí dolní a horní mez
 na číselné ose celých čísel – včetně.
 Pokud tedy zadá 3 a 7, uvažujme čísla 3, 4, 5, 6, 7.
-Vypište pomocí FOR*/
-for (let a = 1; a <=10; a++);
- for (let b = 10; b = b + 10); {
-   console.log("A:" + a);
-   console.log("B:" + b);
-   }  
+Vypište pomocí FOR
+Nechte uživatele zadat horní mez – číslo. Vypište všechny
+čísla od nuly do této horní meze VČETNĚ, které jsou sudá.*/
+let dolni = Number(prompt("dolni mez"));
+let horni = Number(prompt("horni mez"));
+
+
+for (let i = 0; i <= horni; i++) {
+  if (i % 2 ==0) {
+  console.log(i);
   }
+} 
