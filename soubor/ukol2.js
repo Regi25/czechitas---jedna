df0fa683114342abd05b9077253c9f742fb5e272
Regi25 <reginaiuldasheva@gmail.com>
2022-02-24 17:39:12 +0100

    1 file modified, 1 added

---
+++ soubor/ukol2.js
@@ -0,0 +1,7 @@
+let jmeno = prompt("Jake je tvoe jmeno");
+let vek = prompt("Kolik je ti let");
+let rozdilVek = 18 - vek;
+if (vek >= 18) {
+  console.log(jmeno + " " + vek + " " + "a mam pravo ridit auto")
+} else {
+  console.log(jmeno + " " + vek + "a tak musim pockat jeste 4 let");
