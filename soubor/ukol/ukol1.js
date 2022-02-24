513ec3d1dc0e33078afbe11af3cb142aca45acde
Regi25 <reginaiuldasheva@gmail.com>
2022-02-24 17:36:38 +0100

    1 file modified, 1 added, 1 deleted

--- soubor/ukol/ukol1.js
+++
@@ -1,7 +0,0 @@
-let pocitadlo = prompt("Zadej cislo:");
-while (pocitadlo != 0) {
-  console.log("Musi zadat cislo odlisne od 0");
-  pocitadlo = prompt("Zadej cislo:");
-  }
- console.log(pocitadlo);
-}
---
+++ soubor/ukol1.js
@@ -0,0 +1,6 @@
+let jmeno = "Petra";
+let vek = 34;
+let vaha = 65;
+let vyska = 1.67;
+let bmi = vaha / vyska;
+console.log("Jmenuji se" + jmeno + "je mi" + vek + "vazim" + vaha + "kg"+ "a moje BMI je" + bmi");*/
